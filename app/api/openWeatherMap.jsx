8f829add6f50f0e9409a02c3af393693ca6b0ebc
Alex2508git config --global user.name Alex2508 <alexander_beschasnyy@mail.ru>
2017-01-27 16:13:56 +0200

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,19 @@
+var axios = require('axios');
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=bef48d64627b5f65f8b7717209be5d3d&units=metric';
+
+module.exports = {
+    getTemp: function(location){
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(function(res){
+            if(res.data.cod && res.data.message){
+                throw new Error(res.data.message);
+            }else{
+                return res.data.main.temp;
+            }
+        }, function(res){
+            throw new Error(res.data.message);
+        })
+    }
+}
