2a1cbcd1effb34d51484c1b8108adef2611f821c
Alex2508git config --global user.name Alex2508 <alexander_beschasnyy@mail.ru>
2017-02-03 18:30:23 +0200

    Style nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,15 +1,42 @@
 var React = require('react');
 var {Link} = require('react-router');
 
-var Nav = () => {
-    return(
-        <div>
-            <h2>Nav Component</h2>
-            <Link to="/">Get Weather</Link>
-            <Link to="/about">About</Link>
-            <Link to="/examples">Examples</Link>                
-        </div>
-    );
-}
+var Nav = React.createClass({
+    onSearch: function(e){
+        e.preventDefault();
+    },
+    render: function(){
+        return(
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <Link to="/">Get Weather</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples">Examples</Link>                                        
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+})
 
 module.exports = Nav;
