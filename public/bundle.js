c8f43a45d39ace7e5adfa781fbb59544fec9c596
Alex2508git config --global user.name Alex2508 <alexander_beschasnyy@mail.ru>
2017-02-06 19:21:56 +0200

    Add custom styles support

--- public/bundle.js
+++ public/bundle.js
@@ -112,6 +112,9 @@
 	__webpack_require__(257);
 	$(document).foundation();
 
+	// app css
+	__webpack_require__(261);
+
 	ReactDOM.render(React.createElement(
 	    Router,
 	    { history: hashHistory },
@@ -25002,7 +25005,7 @@
 	                        React.createElement(
 	                            'li',
 	                            null,
-	                            React.createElement('input', { type: 'search', placeholder: 'Search weather' })
+	                            React.createElement('input', { type: 'search', placeholder: 'Search weather by city' })
 	                        ),
 	                        React.createElement(
 	                            'li',
@@ -25089,7 +25092,7 @@
 	            null,
 	            React.createElement(
 	                'h1',
-	                { className: 'text-center' },
+	                { className: 'text-center page-title' },
 	                'Get Weather'
 	            ),
 	            React.createElement(WeatherForm, { onSearch: this.handleSearch }),
@@ -25127,7 +25130,7 @@
 	            React.createElement(
 	                'form',
 	                { onSubmit: this.onFormSubmit },
-	                React.createElement('input', { type: 'text', ref: 'location' }),
+	                React.createElement('input', { type: 'search', ref: 'location', placeholder: 'Search weather by city' }),
 	                React.createElement(
 	                    'button',
 	                    { type: 'submit', className: 'button expanded hollow' },
@@ -26752,7 +26755,7 @@
 	        null,
 	        React.createElement(
 	            "h1",
-	            { className: "text-center" },
+	            { className: "text-center page-title" },
 	            "About"
 	        ),
 	        React.createElement(
@@ -26809,7 +26812,7 @@
 	        null,
 	        React.createElement(
 	            'h1',
-	            { className: 'text-center' },
+	            { className: 'text-center page-title' },
 	            'Examples'
 	        ),
 	        React.createElement(
@@ -27191,6 +27194,46 @@
 		if(oldSrc)
 			URL.revokeObjectURL(oldSrc);
 	}
+
+
+/***/ },
+/* 261 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(262);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(260)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../../node_modules/css-loader/index.js!./app.css", function() {
+				var newContent = require("!!./../../node_modules/css-loader/index.js!./app.css");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 262 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(259)();
+	// imports
+
+
+	// module
+	exports.push([module.id, ".page-title{\r\n    margin-top: 2.5rem;\r\n    margin-bottom: 2.5rem;\r\n}\r\n", ""]);
+
+	// exports
 
 
 /***/ }
